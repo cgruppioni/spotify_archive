e9b1d4cdfbc23cfdb7778264cdab5aa6098622d5
Casey Gruppioni <casey@appcues.com>
2018-08-07 20:48:03 -0400

    Loop through tracks and get artists names and track name.

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,5 +1,16 @@
 user_id = ENV['SPOTIFY_USER_ID']
 user = RSpotify::User.find(user_id)
 
-#20 is the max playlists amount
-20_playlists
+
+#20 is the limit
+# TODO learn how to offset or I delete them as I go along
+playlists = user.playlists
+playlist = playlists.first
+tracks = playlist.tracks
+
+tracks.each do |track|
+  track.artists.each do |artist|
+    artist.name
+  end
+  track.name
+end
