0fb658165c6eb5396ce74ba179d2435999bc7e56
Casey Gruppioni <casey@appcues.com>
2018-08-15 22:16:06 -0400

    has_many associations.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,9 @@
 class Artist < ApplicationRecord
-  has_many :tracks
+  has_many :track_artists, dependent: :destroy
+  has_many :tracks, through: :track_artists
   has_many :artist_genres, dependent: :destroy
+  has_many :genres, through: :artist_genres
+
 
   def self.fields
     column_names - ["created_at", "updated_at", "id"]
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ApplicationRecord
+  has_many :artist_genres, dependent: :destroy
+  has_many :artists, through: :artist_genres
+
   def fields
     column_names - ["created_at", "updated_at", "id"]
   end
