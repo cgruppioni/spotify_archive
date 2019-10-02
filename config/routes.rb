c97677f8222d256d6cb70c5241f3079d87c2179e
Casey Gruppioni <casey@appcues.com>
2019-10-02 19:19:38 -0400

    Add WelcomeController & tests -- for react tutorial.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/spotify/callback', to: 'users#spotify'
 
+  resource :welcome, only: [:show]
+
   root 'archives#landing'
 end
